ec026d084d7ec66ae59c0b7dea70b412acd4aa9e
Khushboo <khushboo18aug@gmail.com>
2018-03-31 18:29:01 +0530

    Create api route for login

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import express from 'express';
-const app = express();
+import express from 'express'; // call express
+const app = express(); //init app
+import bcrypt from 'bcryptjs';
 
-import expressValidator from 'express-validator';
 import { check, validationResult } from 'express-validator/check';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 import Config from './config';
 import User from './user.model';
 
@@ -21,42 +22,122 @@ dbConnection.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
+//use middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+const saltRounds = 10;
+
 /**
-* 
-*/
-app.post('/signUp',[
-  check('firstName').exists().withMessage('Name field can not be blank').trim(),
-  check('email').isEmail().withMessage('must be an email').trim(),
-  check('password').exists().withMessage('password can not be blank')
-  ], (req,res) => {
-  console.log('Validating parameters', req);
+ * @api {Post} /signUp Register a new User
+ * @apiVersion 1.0.0
+ * @apiName PostUser
+ * @apiGroup UserRegistration
+ *
+ * @apiParam {String} firstName Mandatory First Name of User
+ * @apiParam {String} [lastName] Last Name of user
+ * @apiParam {String} [country] Country of user
+ * @apiParam {String} email Mandatory EmailId of user
+ * @apiParam {Number} password Mandatory Password
+ *
+ * @apiSuccess (Success 201) - No Content
+ *
+ * @apiError (Error 500) - Internal Server Error
+ */
+app.post('/signUp', [
+  check('firstName').exists().isLength({min:1}).withMessage('Name field can not be blank').trim(),
+  check('email').isEmail().withMessage('Must be an email').trim()
+    .custom(value => {
+      const email = Object.assign({email:value});
+
+      return User.findOne(email)
+      .then(user => {
+        if(user)
+         throw new Error('this email is already in use');
+      })
+    }),
+  check('country').exists().withMessage('Country is required').trim(),
+  check('password', 'passwords must be at least 5 chars long and contain one number').isLength({ min: 5 }).matches(/\d/),
+  ], (req, res) => {
+  console.log('Validating parameters');
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.mapped() });
   }
 
-  User.create(req.body)
+  const data = Object.assign({}, req.body, {
+    password: bcrypt.hashSync(req.body.password, saltRounds),
+  });
+
+  User.create(data)
     .then(() => {
-      console.log('Inserted Successfully');
+      console.log('User registered successfully');
+
       res.sendStatus(201);
     })
-    .catch(() => {
+    .catch(ex => {
       res.sendStatus(500);
-    })
+    });
 });
+
+
+/**
+ * @api {Get} /logIn  User login
+ * @apiVersion 1.0.0
+ * @apiName GetData
+ * @apiGroup UserLogin
+ *
+ * @apiParam {String} email Mandatory EmailId of user
+ * @apiParam {Number} password Mandatory Password
+ *
+ * @apiSuccess (Success 201) - No Content
+ *
+ * @apiError (Error 500) - Internal Server Error
+ */
+app.post('/logIn', [
+  check('email').isEmail().withMessage('must be an email').trim(),
+  check('password').exists().withMessage('password can not be blank')
+  ], (req, res) => {
+  console.log('Validating user credentials');
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.mapped() });
+  }
+
+  const findEmail = Object.assign({email:req.body.email});
+
+  User.findOne(findEmail)
+    .then(user => {
+      const matchedUserHash = user.password || '';
+
+      //compare password with stored decrypted password
+      const compareResult = bcrypt.compareSync(req.body.password, matchedUserHash);
+      if(compareResult) {
+        console.log('Passsword matched successfully');
+        //create token
+        const token = jwt.sign({ sub: user._id }, 'qwertyuirtyu', { algorithm: 'HS384'});
+       
+        res.status(200).send(token);
+      } else {
+        console.log('Passsword matched failed');
+        res.sendStatus(401);
+      }
+    })
+    .catch(ex => {
+      console.log('User not registered with us');
+      res.sendStatus(404);
+    });
+});
+
 
 //server setup
 const server = app.listen(Config.serverPort, () => {
   const port = server.address().port;
   console.log('App is listening on port', port);
 })
-
-
 
 
 
--- src/user.model.js
+++ src/user.model.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const Schema = mongoose.Schema;
+
+//password encryption
+const saltRounds = 10;
 
 const registeredUserSchema = new Schema ({
   firstName: {
@@ -27,24 +31,36 @@ const registeredUserSchema = new Schema ({
 
 const UserModel = mongoose.model('UserModel', registeredUserSchema);
 
-const fns =  {
-  create: (userDetails) => {
-    const user = {
-      'firstName':userDetails.firstName,
-      'lastName':userDetails.lastName,
-      'email':userDetails.email,
-      'country':userDetails.country,
-      'password': userDetails.password,
-    }
 
-    UserModel.insert(user, (err,res) => {
-      if(err) {
-        console.log('failed to insert');
-      } else {
-        console.log('inserted successfully');
-      }
-    })
-  },
-};
+  // create: (userDetails) => {
+
+  //    // Store hash in your password DB.
+  //    //const  salt = bcrypt.genSaltSync(saltRounds); 
+  //    //const  hash = bcrypt.hashSync(userDetails.password, salt);
+  //   const hash = bcrypt.hashSync(userDetails.password, saltRounds);
+  //   console.log(hash);
+
+  //   const user = {
+  //     'firstName':userDetails.firstName,
+  //     'lastName':userDetails.lastName,
+  //     'email':userDetails.email,
+  //     'country':userDetails.country,
+  //     'password': hash
+  //   }
+
+  //   // UserModel.insert(user, (err,res) => {
+  //   //   if(err) {
+  //   //     console.log('Failed to insert in db');
+  //   //   } else {
+  //   //     console.log('Data inserted successfully in db');
+  //   //   }
+  //   // })
+  // },
+
+  // find: (email) => {
+  //  return UserModel.findOne(email);  
+  // },
+
+
 
 module.exports = UserModel;
